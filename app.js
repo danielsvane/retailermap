016a11089d0bbefeeca50ea9ce012a49db34e363
Daniel Svane <danielsvane@gmail.com>
2012-04-18 00:10:55 +0200

    Added procfile

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
+var post = process.env.PORT || 3000;
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
