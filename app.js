7c97b34d00a485470308de2ea89391a0ffc2862d
Daniel Svane <danielsvane@gmail.com>
2012-04-18 00:15:14 +0200

    Fixed port

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-var post = process.env.PORT || 3000;
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
